888925a24d877f758dae6b347b1dc10063d30477
tlatimore <tlatimore0551@yahoo.com>
2025-01-29 08:33:14 -0500

    completed /server todos

--- Develop/server/src/routes/auth-routes.ts
+++ Develop/server/src/routes/auth-routes.ts
@@ -5,6 +5,22 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body; 
+  const user = await User.findOne({ where: { username } });
+
+  if (!user) {
+    return res.status(400).json({ message: 'User not found' });
+  }
+  const valid = await bcrypt.compare(password, user.password);
+  if(!valid) {
+    return res.status(400).json({ message: 'Invalid password' });
+  }
+
+  const jwtKey = process.env.JWT_SECRET_KEY || '';
+
+  const token = jwt.sign({ username }, jwtKey, { expiresIn: '1h' });
+  return res.json({ token });
+
 };
 
 const router = Router();
